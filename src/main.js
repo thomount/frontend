73bcc8b88f10aff2edb89f39ceb48829b1c64d1b
庞博予 <pby17@mails.tsinghua.edu.cn>
2019-11-21 00:11:57 +0800

    change ci

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import store from './store/'
 import ElementUI from 'element-ui'
 import ECharts from 'vue-echarts'
-import QRCode from 'qrcodejs2'
+
 import 'element-ui/lib/theme-default/index.css'
 //import 'echarts/lib/chart/bar'
 //import 'echarts/lib/component/tooltip'
@@ -21,12 +21,4 @@ new Vue({
 	template: '<App/>',
 	components: { App }
 })
-Vue.prototype.$QRCode = function()
-{
-  let qrcode = new QRCode('qrcode', {
-    width: 232,  // 设置宽度
-    height: 232, // 设置高度
-    text: "12345",
-  });
-};
 
